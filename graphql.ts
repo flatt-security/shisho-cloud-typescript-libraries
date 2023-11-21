c5ebb5e7f0eecaaf48f8e36d576f1527ee1e34ba
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2023-11-21 13:43:30 +0900

    release: 2023/11/21

--- graphql.ts
+++ graphql.ts
@@ -11,3 +11,6 @@ export type Float = number;
 export type JSON = string;
 export type AWSRegion = string;
 export type { ResourceID } from "./mod.ts";
+
+// Utilities
+export type Nullable<T> = T | null;
